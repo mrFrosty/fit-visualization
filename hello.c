371999794660f1f964aa15c66cef2236bfe96072
Gunārs Danovskis <danovskisgunars@gmail.com>
2017-10-05 14:54:26 +0300

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>  /* For exit() function */
+int main()
+{
+   char sentence[1000];
+   FILE *fptr;
+
+   fptr = fopen("program.txt", "w");
+   if(fptr == NULL)
+   {
+      printf("Error!");
+      exit(1);
+   }
+
+   fprintf(fptr,"%s", "Hello from FF!");
+   fclose(fptr);
+
+   return 0;
+}
